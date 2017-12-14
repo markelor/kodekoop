1b5f393cd8f8edca2cf14aff242fad98b14198a3
markelor <mendimarkel@gmail.com>
2017-12-14 13:13:48 +0100

    put mantenaince description correct

--- resources/views/maintenance/maintenance.blade.php
+++ resources/views/maintenance/maintenance.blade.php
@@ -1,6 +1,8 @@
 @extends('maintenance')
 @section('title','Kodekoop')
-@section('description', @lang('maintenance.description'))
+@section('description')
+@lang('maintenance.description')
+@endsection
 @section('keywords', 'Trabajos web','Creador web','Paginas web','Diseñador web,Diseño web,Angular,Laravel,PHP,Bootstrap,HTML5,CSS3,JavaScript,Jquery,Freelance,Web Freelancer,SEO,Email marketing,SEO,PPC,Banner, Desarrollador Web, Desarrollo Web')
 
 @section('header')
