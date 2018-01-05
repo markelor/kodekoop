834eed9b001ed992fb03e7c526acf893055b57ae
markelor <mendimarkel@gmail.com>
2018-01-05 12:29:40 +0100

    single service hosting

--- resources/lang/es/services.php
+++ resources/lang/es/services.php
@@ -25,6 +25,20 @@ return array(
     'stats-short-description'=>' Donec id elit non mi porta gravida at eget metus. Fusce dapibus.',
     'photographs-short-description'=>'Donec id elit non mi porta gravida at eget metus. Fusce dapibus.',
     'read-more'=>'Leer Más',
-    'contact-us'=>'Contáctanos'
+    'contact-us'=>'Contáctanos',
+    'hosting-what-is'=>'¿Que es?',
+    'hosting-what-is-description'=>'Web orria garatuta eta prest dugunean, espazio erreserbatu bat behar dugu behar diren osagai guztiak: fitxategiak, argazkiak, datu basea… gordetzeko.
+Honetarako, eta gure web orria bixitatzen duten bezero guztien eskariak zerbitzatzeko, eguneko 24 orduetan lanean ibiliko den makina bat behar dugu. Makina honi zerbitzaria deitzen zaio.
+Zerbitzari honetan, gure web orriari “ostatu” ematea da hostinga. Hostinga, interneten dauden ornitzaileei alokatzen zaie gure bete beharrek asetzeko, makinaren kapazitatea gure eskakizunetara egokitu behar delarik.',
+'hosting-kodekoop'=>'Hosting con Kodekoop.',
+'hosting-share'=>'Compartido',
+'hosting-single'=>'Unitario',
+'hosting-kodekoop-share'=>'Web orria garatuta eta prest dugunean, espazio erreserbatu bat behar dugu behar diren osagai guztiak: fitxategiak, argazkiak, datu basea… gordetzeko.
+Honetarako, eta gure web orria bixitatzen duten bezero guztien eskariak zerbitzatzeko, eguneko 24 orduetan lanean ibiliko den makina bat behar dugu. Makina honi zerbitzaria deitzen zaio.
+Zerbitzari honetan, gure web orriari “ostatu” ematea da hostinga. Hostinga, interneten dauden ornitzaileei alokatzen zaie gure bete beharrek asetzeko, makinaren kapazitatea gure eskakizunetara egokitu behar delarik.',
+'hosting-kodekoop-single'=>'Zerbitzari osoa bezero batentzat soilik erabiliko da. Aukera hau oso interesgarria izan daiteke bezero batek hainbat web orri baditu, kasu hontan, bere proiektu guztiak, solik berea den makina hontan jar ditzazke. Zerbitzu hau erabiltzeko beste arrazoi bat, jarri behar dugun web orriak errekutso asko kontsumitzen dituenean gertatzen da. Web orrian sortzen den trafikoak behar duen banda-zabalera, eta memoria beharra izan daitezke arrazoi batzuk besteak beste.
+Banakako hostingaren kudeaketa, bezeroak berak eman dezake hala nahi badu. Kasu hontan, Kodekoopek ez du bezeroak zerbitzarian egiten dituen aldaketen erantzukizunik bere gain  hartuko, aukera hau oso arriskutsua izan daiteke bezeroak web garapenean jakintzarik ez badu.
+Beste aukera, zerbitzariaren kontrola Kodekoopen eskuetan uztea da eta geuk emango genituzke pauso guztiak denak ongi funtziona dezan.
+Preziori dagokionez, banakako zerbitzaria garestiagoa da, bezeroak berak ordaindu behar duelako zerbitzaria inorekin banatu gabe.',
 
 );
--- resources/lang/eu/services.php
+++ resources/lang/eu/services.php
@@ -24,6 +24,21 @@ return array(
     'stats-short-description'=>' Donec id elit non mi porta gravida at eget metus. Fusce dapibus.',
     'photographs-short-description'=>'Donec id elit non mi porta gravida at eget metus. Fusce dapibus.',
     'read-more'=>'Gehiago Irakurri',
-    'contact-us'=>'Jar zaitez gurekin harremanetan'
+    'contact-us'=>'Jar zaitez gurekin harremanetan',
+    'hosting-what-is'=>'Zer da?',
+    'hosting-what-is-description'=>'Web orria garatuta eta prest dugunean, espazio erreserbatu bat behar dugu behar diren osagai guztiak: fitxategiak, argazkiak, datu basea… gordetzeko.
+Honetarako, eta gure web orria bixitatzen duten bezero guztien eskariak zerbitzatzeko, eguneko 24 orduetan lanean ibiliko den makina bat behar dugu. Makina honi zerbitzaria deitzen zaio.
+Zerbitzari honetan, gure web orriari “ostatu” ematea da hostinga. Hostinga, interneten dauden ornitzaileei alokatzen zaie gure bete beharrek asetzeko, makinaren kapazitatea gure eskakizunetara egokitu behar delarik.',
+'hosting-kodekoop'=>'Hostinga Kodekoopekin.',
+'hosting-share'=>'Elkarbanatua',
+'hosting-single'=>'Banakakoa',
+
+'hosting-kodekoop-share'=>'Hosting elkarbanatua: Hainbat bezeroak zerbitzari bera konpartitu dezakete, honen kapazitatea altua den kasuetan. Bezero bakoitzari espazio jakin bat emango zaio bere web orriak behar dituen baliabideak erabiltzeko eta zerbitzaria bezero guztien artean ordaintzen da.
+Aukera hau, estatikoak eta errekutso gutxi erabiltzen dituzten web orrientzat erabiltzen da batez ere, zerbitzari batean web orri askoren metaketak, errekurtsoak agortu, eta arazoak ekar ditzazkeelako. Memoria arazoak eta abiadura arazoak besteak beste.
+Hosting elkarbanatuaren kasuan, kodekoop arduratuko da bere kudeaketarekin, beti ere zerbitzu ona eskeintzea da gure helburua, web orrien kontrol gabeko metaketarik gerta ez dadin.',
+'hosting-kodekoop-single'=>'Zerbitzari osoa bezero batentzat soilik erabiliko da. Aukera hau oso interesgarria izan daiteke bezero batek hainbat web orri baditu, kasu hontan, bere proiektu guztiak, solik berea den makina hontan jar ditzazke. Zerbitzu hau erabiltzeko beste arrazoi bat, jarri behar dugun web orriak errekutso asko kontsumitzen dituenean gertatzen da. Web orrian sortzen den trafikoak behar duen banda-zabalera, eta memoria beharra izan daitezke arrazoi batzuk besteak beste.
+Banakako hostingaren kudeaketa, bezeroak berak eman dezake hala nahi badu. Kasu hontan, Kodekoopek ez du bezeroak zerbitzarian egiten dituen aldaketen erantzukizunik bere gain  hartuko, aukera hau oso arriskutsua izan daiteke bezeroak web garapenean jakintzarik ez badu.
+Beste aukera, zerbitzariaren kontrola Kodekoopen eskuetan uztea da eta geuk emango genituzke pauso guztiak denak ongi funtziona dezan.
+Preziori dagokionez, banakako zerbitzaria garestiagoa da, bezeroak berak ordaindu behar duelako zerbitzaria inorekin banatu gabe.',
 
 );
